6c7b7f09143717bde34e3869cc90e01c67100237
Neil Agarwal <neilagarwal2713@gmail.com>
2018-01-07 17:46:41 -0500

    Initial Commit

---
+++ Arrive Awake/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Arrive Awake
+//
+//  Created by Neil Agarwal on 2018-01-07.
+//  Copyright © 2018 Neil Agarwal. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Arrive Awake/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Arrive Awake
+//
+//  Created by Neil Agarwal on 2018-01-07.
+//  Copyright © 2018 Neil Agarwal. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
